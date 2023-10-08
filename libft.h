ea19adbd5814f7be327ffe44a1afd7b95ece7c91
yemoreno <yemoreno@student.42malaga.com>
2023-10-08 12:54:43 +0200

    add comments i already have from other projects

--- libft.h
+++ libft.h
@@ -26,6 +26,12 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+/**
+ * @brief	The length of given string.
+ * @return	The length of the string.
+ * 			NULL if nothing given.
+ * @param	char	*str : the string to measure.
+ */
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, size_t c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -35,6 +41,15 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(size_t c);
 int		ft_tolower(size_t c);
+/**
+ * @brief	Searchs in given string for the first aparition
+ * 			of searched_char.
+ * @return	Pointer of string from first aparition of seached_char
+ * 			in string given.
+ * 			NULL if any error or not found.
+ * @param	char	*string : where we will search.
+ * @param	int		searched_char : the char we are searching for.
+ */
 char	*ft_strchr(const char *s, size_t c);
 char	*ft_strrchr(const char *s, size_t c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -44,7 +59,23 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *s1);
+/**
+ * @brief	Given a string, it takes len characters from the position
+ * 			given.
+ * @return	A substring of the given string with the parameters given.
+ * 			NULL if any error.
+ * @param	char	*s : The original string to work.
+ * @param	size_t	start : The position where to start the substring.
+ * @param	size_t	len : The desired length of the substring.
+ */
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+/**
+ * @brief	Join two strings into one.
+ * @return	String resulted from joining the 2 given strings.
+ * 			NULL if error.
+ * @param	char	*s1 : the first string.
+ * @param	char	*s2 : the string to join to s1.
+ */
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(const char *s, char c);
